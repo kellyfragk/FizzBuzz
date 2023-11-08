e20c3be611dd21375c35c8ea7b03f37e43241907
kellyfragk <kelly.fragkogianni@softwire.com>
2023-11-08 11:51:59 +0000

    Part 2 solution & package-lock.json update

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else console.log(i);
+for (let i = 1; i <= 255; i++) {
+  let result = "";
+
+  //conditions
+  if (i % 3 === 0) {
+    result = "Fizz";
+  }
+  if (i % 13 === 0) {
+    result = result + "Fezz";
+  }
+  if (i % 5 === 0) {
+    result = result + "Buzz";
+  }
+  if (i % 7 === 0) {
+    result = result + "Bang";
+  }
+  if (i % 17 === 0 && result !== "") {
+    result = result
+      .match(/.{1,4}/g)
+      .reverse()
+      .join("");
+  }
+
+  // printing
+  if (result !== "") {
+    console.log(result);
+  } else {
+    console.log(i);
+  }
 }
